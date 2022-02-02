4998b20a426dfaf990999ed77064c4102886dc4f
saparian0304 <anjp0304@gmail.com>
2022-02-02 10:50:07 +0900

    타자연습프로그램 : 한줄씩 출력 후 입력하기

--- Personal_project/src/Read_Txt.java
+++ Personal_project/src/Read_Txt.java
@@ -3,18 +3,23 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Read_Txt {
 
 	public static void main(String[] args) throws IOException {
-		File file = new File("C:\\Users\\wnsvy\\Desktop\\programming\\Personal_project\\Joshua_1.txt");
+		File file = new File("C:\\Users\\wnsvy\\Desktop\\programming\\java1\\Personal_project\\Joshua_1.txt");
+
+		// 입력을 받는데 사용하는 클래스
+		Scanner scanner = new Scanner(System.in); 
 		
+		// txt 파일을 읽는데 사용할 클래스
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
 
 		String line = null;
-
 		while ((line = br.readLine()) != null) {
-	        System.out.println(line);
+	        System.out.printf(line + "%n");
+			scanner.nextLine();
 	    }
 		
 
