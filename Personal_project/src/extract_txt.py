22ca1dba90f5fddc9c18dd9fbd49d1d596346cf4
saparian0304 <anjp0304@gmail.com>
2022-02-16 01:51:11 +0900

    원하는 장까지 한줄씩 출력하는 기능
    최종목표 : 장별로 txt 파일 만드는 것.

--- Personal_project/src/extract_txt.py
+++ Personal_project/src/extract_txt.py
@@ -13,13 +13,20 @@ while i < 50 :
 # 장별로 나누기
 import re
 f = open('Personal_project/Gen.txt', 'r')
+chapter = 1
 
-i = 0
-while i < 50 :
+i = 1
+print(str(i)+"장")
+while i < (chapter + 1) :
+    
+    c = format(i, '02')
     readF = f.readline()
-    if readF[1:3] == "01":
-        results = re.sub('^창[0][1]:\d+' ' ',"",readF)
+    if readF[1:3] == str(c):
+        results = re.sub('^창'+str(c)+':\d+' + ' ',"",readF)
         print(results)
-        i += 1
+       
     else:
+        i += 1
+        print(str(i)+"장")
         continue
+        
