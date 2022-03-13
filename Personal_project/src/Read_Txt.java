5d0d3bc482503fe9c8dfbdb7be06c900a2834116
saparian0304 <anjp0304@gmail.com>
2022-03-13 22:56:55 +0900

    개인프로젝트_영어성경쓰기 기능
    영어성경쓰기 기능
    성경책파일 : 절대경로 -> 상대경로
    책, 장 입력 안내문 추가

--- Personal_project/src/Read_Txt.java
+++ Personal_project/src/Read_Txt.java
@@ -8,20 +8,30 @@ import java.util.Scanner;
 public class Read_Txt {
 
 	public static void main(String[] args) throws IOException {
-		File file = new File("C:\\Users\\wnsvy\\Desktop\\programming\\java1\\Personal_project\\bible.txt");
-
 		// 입력을 받는데 사용하는 클래스
 		Scanner scanner = new Scanner(System.in); 
 		
+		System.out.printf("읽을 성경책의 약자를 입력하세요 : ");
+		String name = scanner.nextLine();
+		
+		System.out.printf("원하는 장을 입력하세요 : ");
+		int chapter = Integer.parseInt(scanner.nextLine());
+		
+		int verse = 1;
+		
+		String pathBible = "./bible/%s_en.txt";
+		
+		File file = new File("./bible.txt");
+		File file_en = new File(String.format(pathBible, name));
+		
 		// txt 파일을 읽는데 사용할 클래스
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"EUC_KR"));
-		int verse = 1;
-		String name = scanner.nextLine();
-		int chapter = scanner.nextInt();
+		BufferedReader br_en = new BufferedReader(new InputStreamReader(new FileInputStream(file_en),"EUC_KR"));
 		
 		
 		String line = null;
-		while ((line = br.readLine()) != null) {
+		String line_en = null;
+		while ((line = br.readLine()) != null && (line_en = br_en.readLine()) != null) {
 
 			
 			String s_chap = Integer.toString(chapter);
@@ -29,15 +39,22 @@ public class Read_Txt {
 			
 			while (line.substring(0,name.length()+s_chap.length()).equals(name+Integer.toString(chapter))==false)
 			{
-				line = br.readLine();}
+				line = br.readLine();
+				}			
+			while (line_en.substring(0,s_chap.length()).equals(Integer.toString(chapter))==false)
+			{
+				line_en = br_en.readLine();
+				}
 			
 	        System.out.printf(line.substring(name.length()+s_chap.length()+ s_verse.length() + 2) + "%n");
+	        System.out.printf(line_en.substring(s_chap.length()+ s_verse.length() + 2) + "%n");
 	        String inputTxt = scanner.nextLine();
 			
 	        // 입력값이 맞았는지 확인
-	        while (line.substring(name.length()+s_chap.length()+s_verse.length() + 2).equals(inputTxt) == false) {
+	        while (line_en.substring(s_chap.length()+ s_verse.length() + 2).equals(inputTxt) == false) {
 	        	System.out.println("틀렸습니다.");
 	        	System.out.printf(line.substring(name.length()+s_chap.length()+s_verse.length() + 2) + "%n");
+	        	System.out.printf(line_en.substring(s_chap.length()+ s_verse.length() + 2) + "%n");
 	        	inputTxt = scanner.nextLine();
 	        	continue;
 	        }
@@ -45,6 +62,7 @@ public class Read_Txt {
 	        
 	    }
 		br.close();
+		br_en.close();
 		scanner.close();
 		
 	}
