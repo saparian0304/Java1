e7aad0e68b22e7374d07d66a980f9b3938a426f0
saparian0304 <anjp0304@gmail.com>
2022-03-19 23:13:05 +0900

    자바_조건문_if문_{}사용

---
+++ Personal_project/src/StandardProcedureOfJava/ch4/ch4_FlowEx2.java
@@ -0,0 +1,23 @@
+package StandardProcedureOfJava.ch4;
+
+import java.util.Scanner;
+
+public class ch4_FlowEx2 {
+
+	public static void main(String[] args) {
+		int input;
+		
+		System.out.println("숫자를 하나 입력하세요.>");
+		
+		Scanner scanner = new Scanner(System.in);
+		String tmp = scanner.nextLine();	// 화면을 통해 입력받은 내용을 tmp에 저장
+		input = Integer.parseInt(tmp);		// 입력받은 문자열(tmp)을 숫자로 변환
+		
+		if(input==0) {
+			System.out.println("입력하신 숫자는 0입니다");
+		}
+		if(input!=0)
+			System.out.println("입력하신 숫자는 0이 아닙니다");
+			System.out.printf("입력하신 숫자는 %d입니다.", input);	// {}이 없어서 if문에 속해있지 않는다
+	}	// main의 끝
+}
