512daafc8121642470b31d41453cfbb7d79b9bb5
saparian0304 <anjp0304@gmail.com>
2022-03-21 23:32:59 +0900

    자바_조건문_중첩if문

--- Personal_project/src/StandardProcedureOfJava/ch4/ch4_FlowEx4.java
+++ Personal_project/src/StandardProcedureOfJava/ch4/ch4_FlowEx4.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class ch4_FlowEx4 {
 
 	public static void main(String[] args) {
-		int score = 0;
-		char grade = ' ';
+		int score = 0;		// 점수를 저장하기 위한 변수
+		char grade = ' ';	// 학점을 저장하기 위한 변수. 공백으로 초기화한다.
 		
 		System.out.print("점수를 입력하세요.>");
 		Scanner scanner = new Scanner(System.in);
@@ -18,8 +18,12 @@ public class ch4_FlowEx4 {
 			grade = 'B';
 		} else if (score >= 70) {
 			grade = 'C';
-		} 
+		} else {
+			grade = 'D';
+		}
 		System.out.println("당신의 학점은 "+grade+"입니다.");
 	} // main의 끝
 
 }
+
+// else는 없어도 된다. 없을 경우 if, else if의 조건을 만족하지 않을 경우, 수행문 실행없이 넘어간다. 
